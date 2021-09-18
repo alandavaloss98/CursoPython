cc574fd79474bcc9c843b2d63e9602ed7f3ae30a
alandavaloss98 <alanro32@gmail.com>
2021-09-18 18:19:32 -0500

    Hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,28 @@
+print("Hola mundo")
+
+message1 = "Hola mundo"
+message2 = "Adios mundo"
+
+print(message1)
+print(message2)
+
+print(message2.upper())
+print(message2.lower())
+
+nombre = "Alan"
+apellido = "Davalos"
+
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+
+#Este es un comentario
+nombre_completo_f_string = f"{nombre} {apellido}"
+print(nombre_completo_f_string)
+
+texto = "uno\ndos\ntres"
+print(texto)
+
+mi_mensaje = "        Hola        "
+print(mi_mensaje.lstrip())
+print(mi_mensaje.rstrip())
+print(mi_mensaje.strip())
